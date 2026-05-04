19b2183333eee7d13e7455246e9832ce76e9c656
Phelipe Bistaffa <contato.phelipebistaffa@gmail.com>
2026-05-04 11:38:46 -0400

    Props add

--- backend/src/main/java/ValidarCodigoUsuario/PhelipeProject/service/EnviarEmailService.java
+++ backend/src/main/java/ValidarCodigoUsuario/PhelipeProject/service/EnviarEmailService.java
@@ -20,9 +20,11 @@ public class EnviarEmailService {
 
         Properties props = new Properties();
         props.put("mail.smtp.host", "smtp.gmail.com");
-        props.put("mail.smtp.port", "587");
+        props.put("mail.smtp.port", "465");
         props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.ssl.enable", "true");           // SSL direto
+        props.put("mail.smtp.timeout", "10000");
+        props.put("mail.smtp.connectiontimeout", "10000");
 
         Session session = Session.getInstance(props, new Authenticator() {
             @Override
